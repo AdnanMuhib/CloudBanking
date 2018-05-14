e7f738eaf5931ed206443f44cd97d248d01ca90a
Adnan <adnan.muhib@rocketmail.com>
2018-05-14 23:34:17 +0500

    :100:Dashboard Partial Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.dashboard')
+@section('title','Dashboard')
+@section('body')
 <div class="container">
-    <div class="row justify-content-center">
+    {{-- <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -18,6 +18,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
 @endsection
